f0bd4c62e555fa13029849c7809076938ff66b52
Krissttina <krismetodieva123@gmail.com>
2022-06-24 22:54:45 +0300

    script changed

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # social-media-dublicator
 Dublicate most common function of a social media platform 
-I would choose a twitter function
+I m going to choose a twitter function
 Put instructions here + photo representation
--- script.js
+++ script.js
@@ -1,17 +1,11 @@
 const heart = document.getElementById('hart');
 const hashtag = document.getElementById('hashtag');
 const comment = document.getElementById('comnt');
-
-heart.addEventListener("click", () => {
-    document.addEventListener("click", () => {
-        if("click" % 1 == 1){
-            heart.style.color = "red";
-        }else{
-            heart.style.color = "grey";
-        }
-    })
-})
     
+heart.addEventListener("click", () => {
+        heart.style.color = "red";
+}) 
+
 
 hashtag.addEventListener("click", () => {
     hashtag.style.color = "blue"; 
