7ea205a676fd585be28ca9b0049422f009dbcadd
Kristina <krismetodieva123@gmail.com>
2023-04-23 21:32:52 +0300

    code update
    changed some details and added some new features like the comment section and input fields

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
-const heart = document.getElementById('hart');
+const heart = document.getElementById('heart');
 const hashtag = document.getElementById('hashtag');
-const comment = document.getElementById('comnt');
+const comment = document.getElementById('comment');
+
+const hashtagAdd = document.getElementById('tag');
+const commentAdd = document.getElementById('comm');
     
-function getRed(){
+function onLike(){
     if(heart.style.color == "red"){
         heart.style.color = "black";
     }else{
@@ -10,20 +13,31 @@ function getRed(){
     }
 }
 
-
-function getBlue(){
+function addTag(){
     if(hashtag.style.color == "blue"){
-        hashtag.style.color = "black"; 
+        hashtag.style.color = "black";
+        
     }else{
        hashtag.style.color = "blue"; 
+       hashtagAdd.removeAttribute('hidden');
+    }
+
+    if(hashtag.value){
+        let newtag = hashtagAdd.value;
+        console.log(newtag);
     }
 }
-    
 
-function getMarked(){
+function addComment(){
     if(comment.style.color == "grey"){
         comment.style.color = "black"; 
     }else{
-        comment.style.color = "grey"; 
+        comment.style.color = "grey";
+        commentAdd.removeAttribute('hidden');
+    }
+
+    if(commentAdd.value){
+        let newcom = commentAdd.value;
+        console.log(newcom);
     }
 }
